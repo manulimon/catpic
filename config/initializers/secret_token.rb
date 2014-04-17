4d927a946c605f03a3be0616286971ee6d9595d6
manuligit <riina.vaisanen@gmail.com>
2014-04-18 03:00:18 +0900

    Creating repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catpic::Application.config.secret_key_base = '63e3080efb38a3e0f8dcb7d34f897bfa05388656616f50c15e42150bc4fbbf1872e36bd7383641e9d6fcebb1140fa12b35c8560b69f09aa1f553d10d822c752e'
