166b49fa539cee6817246611fe65adf31c45c3da
manuligit <riina.vaisanen@gmail.com>
2014-04-26 22:40:46 +0900

    Small html changes

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,11 @@
 class Cat < ActiveRecord::Base
   validates :user_id, presence: true
   validates :name, presence: true
+  has_one :user
+
+
+  def to_s
+    "#{name}"
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :pics
+  has_many :cats
   validates :name, uniqueness:true
   validates :password, length: { minimum: 3 }
 
